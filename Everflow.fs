318124d39ea56a07081ebcb64b6585c501f56dcc
Nate Whetsell <nathan.whetsell@gmail.com>
2026-07-24 15:01:33 -0400

    Refactor

--- Everflow.fs
+++ Everflow.fs
@@ -9,6 +9,38 @@
         {
             "NAME" : "inputImage",
             "TYPE" : "image"
+        },
+        {
+            "NAME": "viscosity",
+            "LABEL": "Viscosity",
+            "TYPE": "float",
+            "DEFAULT": 0,
+            "MAX": 10,
+            "MIN": 0
+        },
+        {
+            "NAME": "gravity",
+            "LABEL": "Gravity",
+            "TYPE": "float",
+            "DEFAULT": 0,
+            "MAX": 1,
+            "MIN": -1
+        },
+        {
+            "NAME": "maxSpeed",
+            "LABEL": "Maximum speed",
+            "TYPE": "float",
+            "DEFAULT": 1,
+            "MAX": 10,
+            "MIN": 0
+        },
+        {
+            "NAME": "specularAmount",
+            "LABEL": "Specular amount",
+            "TYPE": "float",
+            "DEFAULT": 3,
+            "MAX": 10,
+            "MIN": 0
         }
     ],
     "ISFVSN": "2",
@@ -60,41 +92,66 @@ float rectSDF(vec2 p, vec2 b) {
 }
 
 
+
+// Hash function from <https://www.shadertoy.com/view/4djSRW>, MIT-licensed:
+//
+// Copyright © 2014 David Hoskins.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+vec3 hash32(vec2 p)
+{
+	vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yxz+33.33);
+    return fract((p3.xxy+p3.yzz)*p3.zyx);
+}
+
+
 //
 // ShaderToy Common
 //
 #define dt 1.5
-
 #define border_h 5.
-
 #define mass 1.
-
 #define fluid_rho 0.5
 
 float Pf(vec2 rho)
 {
-    //return 0.2*rho.x; //gas
-    float GF = 1.;//smoothstep(0.49, 0.5, 1. - rho.y);
-    return mix(0.5*rho.x,0.04*rho.x*(rho.x/fluid_rho - 1.), GF); //water pressure
+    // Water pressure
+    float GF = 1.;
+    return mix(0.5 * rho.x, 0.04 * rho.x * (rho.x / fluid_rho - 1.), GF);
 }
 
 float border(vec2 p)
 {
-    float bound = -rectSDF(p - RENDERSIZE*0.5, RENDERSIZE*vec2(0.5, 0.5));
-    float box = rectSDF(p - RENDERSIZE*vec2(0.5, 0.6), RENDERSIZE*vec2(0.05, 0.01));
-    float drain = -rectSDF(p - RENDERSIZE*vec2(0.5, 0.7), RENDERSIZE*vec2(1.5, 2.5));
-    return max(drain,min(bound, box));
+    float bound = -rectSDF(p - RENDERSIZE * 0.5, RENDERSIZE * vec2(0.5, 0.5));
+    float box = rectSDF(p - RENDERSIZE * vec2(0.5, 0.6), RENDERSIZE * vec2(0.05, 0.01));
+    float drain = -rectSDF(p - RENDERSIZE * vec2(0.5, 0.7), RENDERSIZE * vec2(1.5, 2.5));
+    return max(drain, min(bound, box));
 }
 
 #define h 1.
 vec3 bN(vec2 p)
 {
-    vec3 dx = vec3(-h,0,h);
-    vec4 idx = vec4(-1./h, 0., 1./h, 0.25);
-    vec3 r = idx.zyw*border(p + dx.zy)
-           + idx.xyw*border(p + dx.xy)
-           + idx.yzw*border(p + dx.yz)
-           + idx.yxw*border(p + dx.yx);
+    vec3 r = vec3( 1./h,     0, 0.25) * border(p + vec2( h,  0)) +
+             vec3(-1./h,     0, 0.25) * border(p + vec2(-h,  0)) +
+             vec3(    0,  1./h, 0.25) * border(p + vec2( 0,  h)) +
+             vec3(    0, -1./h, 0.25) * border(p + vec2( 0, -h));
     return vec3(normalize(r.xy), r.z + 1e-4);
 }
 
@@ -127,23 +184,16 @@ particle getParticle(vec4 positionAndMass, vec4 velocity, vec2 pos)
     return P;
 }
 
-vec3 hash32(vec2 p)
-{
-	vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yxz+33.33);
-    return fract((p3.xxy+p3.yzz)*p3.zyx);
-}
-
-//diffusion amount
+// diffusion amount
 vec3 distribution(vec2 x, vec2 p, float K)
 {
     vec4 aabb0 = vec4(p - 0.5, p + 0.5);
-    vec4 aabb1 = vec4(x - K*0.5, x + K*0.5);
+    vec4 aabb1 = vec4(x - K * 0.5, x + K * 0.5);
     vec4 aabbX = vec4(max(aabb0.xy, aabb1.xy), min(aabb0.zw, aabb1.zw));
-    vec2 center = 0.5*(aabbX.xy + aabbX.zw); //center of mass
-    vec2 size = max(aabbX.zw - aabbX.xy, 0.); //only positive
-    float m = size.x*size.y/(K*K); //relative amount
-    //if any of the dimensions are 0 then the mass is 0
+    vec2 center = 0.5 * (aabbX.xy + aabbX.zw); // center of mass
+    vec2 size = max(aabbX.zw - aabbX.xy, 0.); // only positive
+    float m = size.x * size.y / (K * K); // relative amount
+    // if any of the dimensions are 0 then the mass is 0
     return vec3(center, m);
 }
 
@@ -170,7 +220,7 @@ void main()
         // tracking conservative.
         for (int i = -2; i <= 2; i++)
         for (int j = -2; j <= 2; j++) {
-            vec2 translatedPosition = position + vec2(i,j);
+            vec2 translatedPosition = position + vec2(i, j);
             vec2 wrappedPosition = mod(translatedPosition, RENDERSIZE);
 
             particle P0 = getParticle(
@@ -179,45 +229,41 @@ void main()
                 translatedPosition
             );
 
-            P0.X += P0.V*dt; //integrate position
+            P0.X += P0.V * dt; //integrate position
 
-            float difR = 0.9 + 0.21*smoothstep(fluid_rho*0., fluid_rho*0.333, P0.M.x);
+            float difR = 0.9 + 0.21*smoothstep(fluid_rho * 0., fluid_rho * 0.333, P0.M.x);
             vec3 D = distribution(P0.X, position, difR);
-            //the deposited mass into this cell
-            float m = P0.M.x*D.z;
+            // the deposited mass into this cell
+            float m = P0.M.x * D.z;
 
-            //add weighted by mass
-            P.X += D.xy*m;
-            P.V += P0.V*m;
-            P.M.y += P0.M.y*m;
+            // add weighted by mass
+            P.X += D.xy * m;
+            P.V += P0.V * m;
+            P.M.y += P0.M.y * m;
 
-            //add mass
+            // add mass
             P.M.x += m;
         }
 
-        //normalization
-        if(P.M.x != 0.)
-        {
+        // normalization
+        if (P.M.x != 0.) {
             P.X /= P.M.x;
             P.V /= P.M.x;
             P.M.y /= P.M.x;
         }
 
-        //initial condition
-        if(FRAMEINDEX < 1)
-        {
+        // initial condition
+        if (FRAMEINDEX < 1) {
             P.X = position;
 
-            //random
+            // random
             vec3 rand = hash32(position);
-            if(rand.z < 0.2)
-            {
-                P.V = 0.5*(rand.xy-0.5) + vec2(sin(2.*position.x/RENDERSIZE.x), cos(2.*position.x/RENDERSIZE.x));
-                P.M = vec2(mass, 0.5 - 0.5*sin(10.*position.x/RENDERSIZE.x));
+            if(rand.z < 0.2) {
+                P.V = 0.5 * (rand.xy - 0.5) + vec2(sin(2. * position.x / RENDERSIZE.x), cos(2. * position.x / RENDERSIZE.x));
+                P.M = vec2(mass, 0.5 - 0.5 * sin(10. * position.x / RENDERSIZE.x));
             }
-            else
-            {
-                P.V = vec2(0.);
+            else {
+                P.V = vec2(0);
                 P.M = vec2(1e-6);
             }
         }
@@ -238,14 +284,13 @@ void main()
             position
         );
 
-        if(P.M.x != 0.) //not vacuum
-        {
-            //Compute the SPH force
-            vec2 F = vec2(0.);
-            vec3 avgV = vec3(0.);
+        if (P.M.x != 0.) { // not vacuum
+            // Compute the SPH force
+            vec2 F = vec2(0);
+            vec3 avgV = vec3(0);
             for (int i = -2; i <= 2; i++)
             for (int j = -2; j <= 2; j++) {
-                vec2 translatedPosition = position + vec2(i,j);
+                vec2 translatedPosition = position + vec2(i, j);
                 vec2 wrappedPosition = mod(translatedPosition, RENDERSIZE);
                 particle P0 = getParticle(
                     IMG_PIXEL(bufferA_positionAndMass, wrappedPosition),
@@ -253,43 +298,42 @@ void main()
                     translatedPosition
                 );
                 vec2 dx = P0.X - P.X;
-                float avgP = 0.5*P0.M.x*(Pf(P.M) + Pf(P0.M));
-                F -= 0.5*gaussian(dx, INV_SQRT_2)*avgP*dx;
-                avgV += P0.M.x*gaussian(dx, INV_SQRT_2)*vec3(P0.V,1.);
+                float avgP = 0.5 * P0.M.x * (Pf(P.M) + Pf(P0.M));
+                F -= 0.5 * gaussian(dx, INV_SQRT_2) * avgP * dx;
+                avgV += P0.M.x * gaussian(dx, INV_SQRT_2) * vec3(P0.V, 1);
             }
             avgV.xy /= avgV.z;
 
-            //viscosity
-            F += 0.*P.M.x*(avgV.xy - P.V);
+            // viscosity
+            F += viscosity * P.M.x * (avgV.xy - P.V);
 
-            //gravity
-           // F += P.M.x*vec2(0., -0.0004);
+            // gravity
+            F += P.M.x * vec2(0, gravity);
 
-            // if(Mouse.z > 0.)
-            // {
-            //     vec2 dm =(Mouse.xy - Mouse.zw)/10.;
-            //     float d = distance(Mouse.xy, P.X)/20.;
-            //     F += 0.001*dm*exp(-d*d);
-            //    // P.M.y += 0.1*exp(-40.*d*d);
+            // if (Mouse.z > 0.) {
+            //     vec2 dm =(Mouse.xy - Mouse.zw) / 10.;
+            //     float d = distance(Mouse.xy, P.X) / 20.;
+            //     F += 0.001 * dm * exp(-d * d);
             // }
 
             //integrate
-            P.V += F*dt/P.M.x;
+            P.V += F * dt / P.M.x;
 
             //border
             vec3 N = bN(P.X);
-            float vdotN = step(N.z, border_h)*dot(-N.xy, P.V);
-            P.V += 0.5*(N.xy*vdotN + N.xy*abs(vdotN));
-            P.V += 0.*P.M.x*N.xy*step(abs(N.z), border_h)*exp(-N.z);
+            float vdotN = step(N.z, border_h) * dot(-N.xy, P.V);
+            P.V += 0.5 * (N.xy * vdotN + N.xy * abs(vdotN));
 
-            if(N.z < 0.) P.V = vec2(0.);
+            if (N.z < 0.) {
+                P.V = vec2(0);
+            }
 
-
-            //velocity limit
+            // velocity limit
             float v = length(P.V);
-            P.V /= (v > 1.)?v:1.;
+            if (v > maxSpeed) {
+                P.V /= v;
+            }
         }
-
 
         gl_FragColor = vec4(PRE_PACK(P.V), 0, 1);
     }
@@ -302,11 +346,11 @@ void main()
             position
         );
 
-        //particle render
-        vec4 rho = vec4(0.);
+        // particle render
+        vec4 rho = vec4(0);
         for (int i = -2; i <= 2; i++)
         for (int j = -2; j <= 2; j++) {
-            vec2 translatedPosition = position + vec2(i,j);
+            vec2 translatedPosition = position + vec2(i, j);
             vec2 wrappedPosition = mod(translatedPosition, RENDERSIZE);
             particle P0 = getParticle(
                 IMG_PIXEL(bufferA_positionAndMass, wrappedPosition),
@@ -314,9 +358,8 @@ void main()
                 translatedPosition
             );
 
-            vec2 x0 = P0.X; //update position
-            //how much mass falls into this pixel
-            rho += 1.*vec4(P.V, P.M)*gaussian(position - x0, INV_SQRT_2);
+            // how much mass falls into this pixel
+            rho += vec4(P.V, P.M) * gaussian(position - P0.X, INV_SQRT_2);
         }
 
         gl_FragColor = rho;
@@ -330,31 +373,32 @@ void main()
             position
         );
 
-        //border render
+        // border render
         vec3 Nb = bN(P.X);
-        float bord = smoothstep(2.*border_h,border_h*0.5,border(position));
+        float bord = smoothstep(2. * border_h, border_h * 0.5, border(position));
 
         vec4 rho = V(position);
-        vec3 dx = vec3(-1., 0., 1.);
-        vec4 grad = -0.5*vec4(V(position + dx.zy).zw - V(position + dx.xy).zw,
+        vec3 dx = vec3(-1, 0, 1);
+        vec4 grad = -0.5 * vec4(V(position + dx.zy).zw - V(position + dx.xy).zw,
                                 V(position + dx.yz).zw - V(position + dx.yx).zw);
-        vec2 N = pow(length(grad.xz),0.2)*normalize(grad.xz+1e-5);
+        vec2 N = pow(length(grad.xz), 0.2) * normalize(grad.xz + 1e-5);
         vec3 n = normalize(vec3(N, 1));
-        vec3 r = reflect(vec3(0,0,1),n);
-        // vec3 specular = 0.7*pow(texture(iChannel0, r).xyz,vec3(2.));//pow(max(dot(N, polar2cart(vec2(1.4, 1))), 0.), 3.5);
-        float specularb = 0.*gaussian(0.4*(Nb.zz - border_h), INV_SQRT_2)*pow(max(dot(Nb.xy, polar2cart(vec2(1.4, 1))), 0.), 3.);
+        vec3 r = reflect(vec3(0, 0, 1), n);
+        float specularb = gaussian(0.4 * (Nb.zz - border_h), INV_SQRT_2) *
+                          pow(max(dot(Nb.xy, polar2cart(vec2(1.4, 1))), 0.), 3.);
 
-        float a = pow(smoothstep(fluid_rho*0., fluid_rho*2., rho.z),0.1);
-        float b = exp(-1.7*smoothstep(fluid_rho*1., fluid_rho*7.5, rho.z));
-        vec3 col0 = vec3(1., 0.5, 0.);
-        vec3 col1 = vec3(0.1, 0.4, 1.);
+        float a = pow(smoothstep(fluid_rho * 0., fluid_rho * 2., rho.z), 0.1);
+        float b = exp(-1.7 * smoothstep(fluid_rho * 1., fluid_rho * 7.5, rho.z));
+        vec3 col0 = vec3(1, 0.5, 0);
+        vec3 col1 = vec3(0.1, 0.4, 1);
+
         // Output to screen
 #ifndef VIDEOSYNC
 #define tanh(x) (2. / (1. + exp(-2. * (x))) - 1.)
 #endif
-        float c = tanh(3.*(rho.w - 1.))*0.5 + 0.5;
-        gl_FragColor.xyz = mix(col0, col1, c)*(1.5*b + specularb*3.)*a;
-        gl_FragColor.xyz = tanh(gl_FragColor.xyz*gl_FragColor.xyz);
+        float c = tanh(3. * (rho.w - 1.)) * 0.5 + 0.5;
+        gl_FragColor.xyz = mix(col0, col1, c) * (1.5 * b + specularb * specularAmount) * a;
+        gl_FragColor.xyz = tanh(gl_FragColor.xyz * gl_FragColor.xyz);
         gl_FragColor.a = 1.;
     }
 }
