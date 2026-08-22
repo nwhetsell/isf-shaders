5c5885d4ed18be988126dfa0bddc1a50bfdc8880
Nate Whetsell <nathan.whetsell@gmail.com>
2026-08-22 08:37:04 -0400

    Remove macros

--- circle dithering.fs
+++ circle dithering.fs
@@ -12,10 +12,6 @@
     ],
     "ISFVSN": "2"
 }*/
-#define O gl_FragColor
-#define U gl_FragCoord.xy
-#define iResolution RENDERSIZE
-#define iTime TIME
 // ref image: http://www.boredpanda.com/single-line-plotter-scribbles-sergej-stoppel/
 // ( doing it simpler: circles instead of scribbles ;-) )
 float L = 8., // L*T = neightborhood size
@@ -24,22 +20,22 @@ float L = 8., // L*T = neightborhood size
 #define T(U) texture(inputImage, (U)/R).r
 //#define T(U) sqrt( texture(inputImage, (U)/R).r * 1.4 )
 //#define T(U) length(texture(inputImage, (U)/R).rgb)
-#define rnd(P) fract( sin( dot(P,vec2(12.1,31.7)) + 0.*iTime )*43758.5453123)
+#define rnd(P) fract( sin( dot(P,vec2(12.1,31.7)) + 0.*TIME )*43758.5453123)
 #define rnd2(P) fract( sin( (P) * mat2(12.1,-37.4,-17.3,31.7) )*43758.5453123)
 #define C(U,P,r) smoothstep(1.5,0.,abs(length(P-U)-r))
 //#define C(U,P,r) exp(-.5*dot(P-U,P-U)/(r*r)) * sin(1.5*6.28*length(P-U)/r) // Gabor
 void main()
 {
-    vec2 R = iResolution.xy;
-    O = vec4(1);
-    for (float j = -L; j <=L; j++) // test potential circle centers in a window around U
-    for (float i = -L; i <=L; i++) {
-        vec2 P = floor( U/T + vec2(i,j) ) *T; // potential circle center
-        P += T*(rnd2(P)-.5);
+    vec2 R = RENDERSIZE.xy;
+    gl_FragColor = vec4(1);
+    for (float j = -L; j <= L; j++) // test potential circle centers in a window around gl_FragCoord
+    for (float i = -L; i <= L; i++) {
+        vec2 P = floor(gl_FragCoord.xy / T + vec2(i,j)) * T; // potential circle center
+        P += T * (rnd2(P) - 0.5);
         float v = T(P); // target grey value
-        float r = mix(2., L*T ,v); // target radius
-        if ( rnd(P) < (1.-v)/ r*4.*d /L*T*T ) { // draw circle with probability
-            O -= C(U,P,r)*.2 ; // * (1.-texture(iChannel0, (U)/R)); // colored variant
+        float r = mix(2., L * T, v); // target radius
+        if (rnd(P) < ((1. - v) / r) * 4. * d/L * T*T) { // draw circle with probability
+            gl_FragColor -= C(gl_FragCoord.xy, P, r) * 0.2 ; // * (1.-texture(iChannel0, (U)/R)); // colored variant
         }
     }
 }
