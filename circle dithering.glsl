b58b4ffd2483b9b38e6af3adc1c2e40d46250658
Nate Whetsell <nathan.whetsell@gmail.com>
2026-08-22 09:12:46 -0400

    Add inputs

--- circle dithering.glsl
+++ circle dithering.glsl
@@ -8,10 +8,39 @@
         {
             "NAME" : "inputImage",
             "TYPE" : "image"
+        },
+        {
+            "NAME": "L",
+            "LABEL": "Search distance",
+            "TYPE": "float",
+            "DEFAULT": 8,
+            "MAX": 30,
+            "MIN": 1
+        },
+        {
+            "NAME": "T",
+            "LABEL": "Grid step",
+            "TYPE": "float",
+            "DEFAULT": 4,
+            "MAX": 100,
+            "MIN": 1
+        },
+        {
+            "NAME": "d",
+            "LABEL": "Density",
+            "TYPE": "float",
+            "DEFAULT": 1,
+            "MAX": 100,
+            "MIN": 0
         }
     ],
     "ISFVSN": "2"
 }*/
+
+// #define ISF_EDITOR_WEBSITE
+#ifdef ISF_EDITOR_WEBSITE
+#define L 8.
+#endif
 
 #include "lygia/color/luminance.glsl"
 #include "lygia/generative/random.glsl" // LYGIA’s functions aren’t exactly the same as the RNG in the Shadertoy shader.
@@ -19,16 +48,11 @@
 // ref image: http://www.boredpanda.com/single-line-plotter-scribbles-sergej-stoppel/
 // ( doing it simpler: circles instead of scribbles ;-) )
 
-float L = 8.,                   // L*T = neightborhood size
-      T = 4.,                   // grid step for circle centers
-      d = 1.;                   // density
-
 #define C(U,P,r) smoothstep(1.5, 0., abs(length(P - U) - r))                       // ring
 //#define C(U,P,r) exp(-.5*dot(P-U,P-U)/(r*r)) * sin(1.5*6.28*length(P-U)/r) // Gabor
 
 void main()
 {
-    vec2 R = RENDERSIZE.xy;
     gl_FragColor = vec4(1);
 
     for (float j = -L; j <= L; j++)    // test potential circle centers in a window around gl_FragCoord
@@ -38,7 +62,7 @@ void main()
         float v = luminance(IMG_PIXEL(inputImage, P)); // target grey value
         float r = mix(2., L * T, v); // target radius
         if (random(P) < ((1. - v) / r) * 4. * d/L * T*T) { // draw circle with probability
-            gl_FragColor -= C(gl_FragCoord.xy, P, r) * 0.2;
+            gl_FragColor.rgb -= C(gl_FragCoord.xy, P, r) * 0.2;
         }
     }
 }
