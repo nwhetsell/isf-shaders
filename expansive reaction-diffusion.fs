752166a689c5ce9494ab4309cae72d6a65e6ae91
Nate Whetsell <nathan.whetsell@gmail.com>
2026-09-14 10:14:21 -0400

    Add light

--- expansive reaction-diffusion.fs
+++ expansive reaction-diffusion.fs
@@ -9,6 +9,19 @@
         {
             "NAME": "inputImage",
             "TYPE": "image"
+        },
+        {
+            "NAME": "showLightWithMouse",
+            "LABEL": "Show light with mouse",
+            "TYPE": "bool",
+            "DEFAULT": false
+        },
+        {
+            "NAME": "mouse",
+            "TYPE": "point2D",
+            "DEFAULT": [0.5, 0.5],
+            "MIN": [0, 0],
+            "MAX": [1, 1]
         }
     ],
     "ISFVSN": "2",
@@ -33,35 +46,7 @@
         }
     ]
 }*/
-vec4 texture(sampler2D, vec2);
-vec2 complex_multiply(in vec2 v1, in vec2 v2)
-{
-    return vec2(
-        v1.x * v2.x - v1.y * v2.y,
-        v1.x * v2.y + v1.y * v2.x
-    );
-}
-float complex_magnitudeSquared(in vec2 v)
-{
-    return v.x * v.x + v.y * v.y;
-}
-float complex_magnitude(in vec2 v)
-{
-    return length(v);
-}
-vec2 complex_conjugate(in vec2 v)
-{
-    return vec2(v.x, -v.y);
-}
-// https://en.wikipedia.org/wiki/Complex_number#Complex_conjugate,_absolute_value,_argument_and_division
-vec2 complex_divide(in vec2 v1, in vec2 v2)
-{
-    return complex_multiply(v1, complex_conjugate(v2)) / complex_magnitudeSquared(v2);
-}
-float complex_argument(in vec2 v)
-{
-    return atan(v.y, v.x);
-}
+// #define ISF_EDITOR_WEBSITE
 /*
 contributors: ["Patricio Gonzalez Vivo", "David Hoskins", "Inigo Quilez"]
 description: Pass a value and get some random normalize value between 0 and 1
@@ -200,7 +185,6 @@ license:
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Prosperity License - https://prosperitylicense.com/versions/3.0.0
     - Copyright (c) 2021 Patricio Gonzalez Vivo under Patron License - https://lygia.xyz/license
 */
-
 /*
 contributors: Patricio Gonzalez Vivo
 description: 'Fix the aspect ratio of a space keeping things squared for you.'
@@ -218,69 +202,12 @@ vec2 aspect(vec2 st, vec2 s) {
 
 // main reaction-diffusion loop
 // actually the diffusion is realized as a separated two-pass Gaussian blur kernel and is stored in buffer C
-float circle(vec2 uv, vec2 aspect, float scale){
-    return clamp( 1. - length((uv-0.5)*aspect*scale), 0., 1.);
-}
-float sigmoid(float x) {
-    return 2./(1. + exp2(-x)) - 1.;
-}
-float conetip(vec2 uv, vec2 pos, float size, float min)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-    return max( min, 1. - length((uv - pos) * aspect / size) );
-}
-float warpFilter(vec2 uv, vec2 pos, float size, float ramp)
-{
-    return 0.5 + sigmoid( conetip(uv, pos, size, -16.) * ramp) * 0.5;
-}
-vec2 vortex_warp(vec2 uv, vec2 pos, float size, float ramp, vec2 rot)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-    vec2 pos_correct = 0.5 + (pos - 0.5);
-    vec2 rot_uv = pos_correct + complex_multiply((uv - pos_correct)*aspect, rot)/aspect;
-    float _filter = warpFilter(uv, pos_correct, size, ramp);
-    return mix(uv, rot_uv, _filter);
-}
-vec2 vortex_pair_warp(vec2 uv, vec2 pos, vec2 vel)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-    float ramp = 5.;
-    float d = 0.2;
-    float l = length(vel);
-    vec2 p1 = pos;
-    vec2 p2 = pos;
-    if(l > 0.){
-        vec2 normal = normalize(vel.yx * vec2(-1., 1.))/aspect;
-        p1 = pos - normal * d / 2.;
-        p2 = pos + normal * d / 2.;
-    }
-    float w = l / d * 2.;
-    // two overlapping rotations that would annihilate when they were not displaced.
-    vec2 circle1 = vortex_warp(uv, p1, d, ramp, vec2(cos(w),sin(w)));
-    vec2 circle2 = vortex_warp(uv, p2, d, ramp, vec2(cos(-w),sin(-w)));
-    return (circle1 + circle2) / 2.;
-}
-// vec2 mouseDelta(){
-//     vec2 pixelSize = 1. / RENDERSIZE;
-//     float eighth = 1./8.;
-//     vec4 oldMouse = IMG_NORM_PIXEL(iChannel2, vec2(7.5 * eighth, 2.5 * eighth));
-//     vec4 nowMouse = vec4(iMouse.xy / RENDERSIZE, iMouse.zw / RENDERSIZE);
-//     if(oldMouse.z > pixelSize.x && oldMouse.w > pixelSize.y &&
-//        nowMouse.z > pixelSize.x && nowMouse.w > pixelSize.y)
-//     {
-//         return nowMouse.xy - oldMouse.xy;
-//     }
-//     return vec2(0.);
-// }
 void main()
 {
     vec2 pixelSize = 1. / RENDERSIZE;
     vec2 uv = gl_FragCoord.xy * pixelSize;
     if (PASSINDEX == 0) // Shadertoy Buffer A
     {
-        // vec2 mouseV = mouseDelta();
-        vec2 aspect = vec2(1, RENDERSIZE.y / RENDERSIZE.x);
-        // uv = vortex_pair_warp(uv, iMouse.xy*pixelSize, mouseV*aspect*1.4);
         vec4 noise = random4(uv + fract(vec2(42,56)*TIME));
         // get the gradients from the blurred image
         vec2 d = pixelSize*4.;
@@ -312,26 +239,26 @@ void main()
     else // Shadertoy Image
     {
         vec2 aspect = vec2(1, RENDERSIZE.y / RENDERSIZE.x);
-        vec4 noise = random4(uv + fract(vec2(42,56)*TIME));
-        vec2 lightSize=vec2(4.);
         // add the pixel gradients
         vec2 d = pixelSize*1.;
         vec4 dx = IMG_NORM_PIXEL(bufferA, uv + vec2(1,0)*d) - IMG_NORM_PIXEL(bufferA, uv - vec2(1,0)*d);
         vec4 dy = IMG_NORM_PIXEL(bufferA, uv + vec2(0,1)*d) - IMG_NORM_PIXEL(bufferA, uv - vec2(0,1)*d);
-        vec2 displacement = vec2(dx.x,dy.x)*lightSize; // using only the red gradient as displacement vector
-        float light = pow(
-            max(1. - distance(0.5 + (uv - 0.5) * aspect * lightSize + displacement, 0.5 + (vec2(0).xy * pixelSize - 0.5) * aspect * lightSize), 0.),
-            4.
-        );
         // recolor the red channel
-        vec4 rd = vec4(IMG_NORM_PIXEL(bufferA,uv+vec2(dx.x,dy.x)*pixelSize*8.).x)*vec4(0.7,1.5,2.0,1.0)-vec4(0.3,1.0,1.0,1.0);
+        gl_FragColor = vec4(IMG_NORM_PIXEL(bufferA,uv+vec2(dx.x,dy.x)*pixelSize*8.).x)*vec4(0.7,1.5,2.0,1.0)-vec4(0.3,1.0,1.0,1.0);
         // and add the light map
-        // gl_FragColor = mix(
-        //     rd,
-        //     vec4(8, 6, 2, 1),
-        //     light * 0.75 * vec4(1. - IMG_NORM_PIXEL(bufferA, uv + vec2(dx.x, dy.x) * pixelSize * 8.).x)
-        // );
-        gl_FragColor = rd;
+        if (showLightWithMouse) {
+            vec2 lightSize = vec2(4);
+            vec2 displacement = vec2(dx.x, dy.x) * lightSize; // using only the red gradient as displacement vector
+            float light = pow(
+                max(1. - distance(0.5 + (uv - 0.5) * aspect * lightSize + displacement, 0.5 + (mouse.xy - 0.5) * aspect * lightSize), 0.),
+                4.
+            );
+            gl_FragColor = mix(
+                gl_FragColor,
+                vec4(8, 6, 2, 1),
+                light * 0.75 * vec4(1. - IMG_NORM_PIXEL(bufferA, uv + vec2(dx.x, dy.x) * pixelSize * 8.).x)
+            );
+        }
         gl_FragColor.a = 1.;
     }
 }
--- expansive reaction-diffusion.glsl
+++ expansive reaction-diffusion.glsl
@@ -9,6 +9,19 @@
         {
             "NAME": "inputImage",
             "TYPE": "image"
+        },
+        {
+            "NAME": "showLightWithMouse",
+            "LABEL": "Show light with mouse",
+            "TYPE": "bool",
+            "DEFAULT": false
+        },
+        {
+            "NAME": "mouse",
+            "TYPE": "point2D",
+            "DEFAULT": [0.5, 0.5],
+            "MIN": [0, 0],
+            "MAX": [1, 1]
         }
     ],
     "ISFVSN": "2",
@@ -34,12 +47,11 @@
     ]
 }*/
 
-#define ISF_EDITOR_WEBSITE
+// #define ISF_EDITOR_WEBSITE
 #ifdef ISF_EDITOR_WEBSITE
 vec4 texture(sampler2D, vec2);
 #endif
 
-#include "complex/complex.glsl"
 #include "lygia/generative/random.glsl"
 #define SAMPLER_FNC(TEX, UV) texture(TEX, UV)
 #define GAUSSIANBLUR1D_SAMPLER_FNC(TEX, UV) IMG_NORM_PIXEL(TEX, UV)
@@ -47,81 +59,11 @@ vec4 texture(sampler2D, vec2);
 #define VERTICAL_COORDINATE_FNC(UV, SHIFT) fract(vec2(UV.x, UV.y + (SHIFT)))
 #include "lygia/filter/gaussianBlur/1D.glsl"
 #include "lygia/math/const.glsl"
-#define INV_HALF_PI (0.5 * INV_PI)
 #include "lygia/space/aspect.glsl"
 
 // main reaction-diffusion loop
 
 // actually the diffusion is realized as a separated two-pass Gaussian blur kernel and is stored in buffer C
-
-float circle(vec2 uv, vec2 aspect, float scale){
-    return clamp( 1. - length((uv-0.5)*aspect*scale), 0., 1.);
-}
-
-float sigmoid(float x) {
-    return 2./(1. + exp2(-x)) - 1.;
-}
-
-float conetip(vec2 uv, vec2 pos, float size, float min)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-    return max( min, 1. - length((uv - pos) * aspect / size) );
-}
-
-float warpFilter(vec2 uv, vec2 pos, float size, float ramp)
-{
-    return 0.5 + sigmoid( conetip(uv, pos, size, -16.) * ramp) * 0.5;
-}
-
-vec2 vortex_warp(vec2 uv, vec2 pos, float size, float ramp, vec2 rot)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-
-    vec2 pos_correct = 0.5 + (pos - 0.5);
-    vec2 rot_uv = pos_correct + complex_multiply((uv - pos_correct)*aspect, rot)/aspect;
-    float _filter = warpFilter(uv, pos_correct, size, ramp);
-    return mix(uv, rot_uv, _filter);
-}
-
-vec2 vortex_pair_warp(vec2 uv, vec2 pos, vec2 vel)
-{
-    vec2 aspect = vec2(1., RENDERSIZE.y / RENDERSIZE.x);
-    float ramp = 5.;
-
-    float d = 0.2;
-
-    float l = length(vel);
-    vec2 p1 = pos;
-    vec2 p2 = pos;
-
-    if(l > 0.){
-        vec2 normal = normalize(vel.yx * vec2(-1., 1.))/aspect;
-        p1 = pos - normal * d / 2.;
-        p2 = pos + normal * d / 2.;
-    }
-
-    float w = l / d * 2.;
-
-    // two overlapping rotations that would annihilate when they were not displaced.
-    vec2 circle1 = vortex_warp(uv, p1, d, ramp, vec2(cos(w),sin(w)));
-    vec2 circle2 = vortex_warp(uv, p2, d, ramp, vec2(cos(-w),sin(-w)));
-    return (circle1 + circle2) / 2.;
-}
-
-// vec2 mouseDelta(){
-//     vec2 pixelSize = 1. / RENDERSIZE;
-//     float eighth = 1./8.;
-//     vec4 oldMouse = IMG_NORM_PIXEL(iChannel2, vec2(7.5 * eighth, 2.5 * eighth));
-//     vec4 nowMouse = vec4(iMouse.xy / RENDERSIZE, iMouse.zw / RENDERSIZE);
-//     if(oldMouse.z > pixelSize.x && oldMouse.w > pixelSize.y &&
-//        nowMouse.z > pixelSize.x && nowMouse.w > pixelSize.y)
-//     {
-//         return nowMouse.xy - oldMouse.xy;
-//     }
-//     return vec2(0.);
-// }
-
-#define iMouse vec2(0)
 
 void main()
 {
@@ -130,10 +72,6 @@ void main()
 
     if (PASSINDEX == 0) // Shadertoy Buffer A
     {
-        // vec2 mouseV = mouseDelta();
-        vec2 aspect = vec2(1, RENDERSIZE.y / RENDERSIZE.x);
-        // uv = vortex_pair_warp(uv, iMouse.xy*pixelSize, mouseV*aspect*1.4);
-
         vec4 noise = random4(uv + fract(vec2(42,56)*TIME));
 
         // get the gradients from the blurred image
@@ -172,32 +110,30 @@ void main()
     {
         vec2 aspect = vec2(1, RENDERSIZE.y / RENDERSIZE.x);
 
-        vec4 noise = random4(uv + fract(vec2(42,56)*TIME));
-
-        vec2 lightSize=vec2(4.);
-
         // add the pixel gradients
         vec2 d = pixelSize*1.;
         vec4 dx = IMG_NORM_PIXEL(bufferA, uv + vec2(1,0)*d) - IMG_NORM_PIXEL(bufferA, uv - vec2(1,0)*d);
         vec4 dy = IMG_NORM_PIXEL(bufferA, uv + vec2(0,1)*d) - IMG_NORM_PIXEL(bufferA, uv - vec2(0,1)*d);
 
-        vec2 displacement = vec2(dx.x,dy.x)*lightSize; // using only the red gradient as displacement vector
-        float light = pow(
-            max(1. - distance(0.5 + (uv - 0.5) * aspect * lightSize + displacement, 0.5 + (iMouse.xy * pixelSize - 0.5) * aspect * lightSize), 0.),
-            4.
-        );
-
         // recolor the red channel
-        vec4 rd = vec4(IMG_NORM_PIXEL(bufferA,uv+vec2(dx.x,dy.x)*pixelSize*8.).x)*vec4(0.7,1.5,2.0,1.0)-vec4(0.3,1.0,1.0,1.0);
+        gl_FragColor = vec4(IMG_NORM_PIXEL(bufferA,uv+vec2(dx.x,dy.x)*pixelSize*8.).x)*vec4(0.7,1.5,2.0,1.0)-vec4(0.3,1.0,1.0,1.0);
 
         // and add the light map
-        // gl_FragColor = mix(
-        //     rd,
-        //     vec4(8, 6, 2, 1),
-        //     light * 0.75 * vec4(1. - IMG_NORM_PIXEL(bufferA, uv + vec2(dx.x, dy.x) * pixelSize * 8.).x)
-        // );
+        if (showLightWithMouse) {
+            vec2 lightSize = vec2(4);
 
-        gl_FragColor = rd;
+            vec2 displacement = vec2(dx.x, dy.x) * lightSize; // using only the red gradient as displacement vector
+            float light = pow(
+                max(1. - distance(0.5 + (uv - 0.5) * aspect * lightSize + displacement, 0.5 + (mouse.xy - 0.5) * aspect * lightSize), 0.),
+                4.
+            );
+
+            gl_FragColor = mix(
+                gl_FragColor,
+                vec4(8, 6, 2, 1),
+                light * 0.75 * vec4(1. - IMG_NORM_PIXEL(bufferA, uv + vec2(dx.x, dy.x) * pixelSize * 8.).x)
+            );
+        }
 
         gl_FragColor.a = 1.;
     }
