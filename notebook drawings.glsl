9af76df565d811fc37c950de4bc78a10090476c5
Nate Whetsell <nathan.whetsell@gmail.com>
2026-09-16 17:26:48 -0400

    Refactor

--- notebook drawings.glsl
+++ notebook drawings.glsl
@@ -20,83 +20,66 @@
 #include "lygia/space/polar2cart.glsl"
 
 
-vec4 getCol(vec2 pos)
+vec4 getColHT(vec2 position)
 {
-    // take aspect ratio into account
-    vec2 uv = ((pos - RENDERSIZE.xy * 0.5) / RENDERSIZE.y * RENDERSIZE.y) / RENDERSIZE.xy + 0.5;
-    vec4 c1 = texture(inputImage, uv);
-    vec4 e = smoothstep(vec4(-0.05), vec4(0), vec4(uv, vec2(1) - uv));
-    // c1 = mix(vec4(1, 1, 1, 0), c1, e.x * e.y * e.z * e.w);
-    float d = clamp(dot(c1.xyz, vec3(-0.5, 1., -0.5)), 0., 1.);
-    vec4 c2 = vec4(0.7);
-    return c1; // min(mix(c1, c2, 1.8 * d), 0.7);
+ 	return smoothstep(0.95, 1.05, IMG_PIXEL(inputImage, position) * 0.8 + 0.2 + random4(position * 0.7));
 }
 
-vec4 getColHT(vec2 pos)
+float averageRGB(vec2 position)
 {
- 	return smoothstep(0.95, 1.05, getCol(pos) * 0.8 + 0.2 + random4(pos * 0.7));
+ 	return dot(IMG_PIXEL(inputImage, position).xyz, vec3(1. / 3.));
 }
 
-float getVal(vec2 pos)
-{
-    vec4 c = getCol(pos);
- 	return pow(dot(c.xyz, vec3(0.333)), 1.) * 1.; // TODO
-}
-
-vec2 getGrad(vec2 pos, float eps)
+vec2 getGrad(vec2 position, float eps)
 {
    	vec2 d = vec2(eps, 0);
     return vec2(
-        getVal(pos + d.xy) - getVal(pos - d.xy),
-        getVal(pos + d.yx) - getVal(pos - d.yx)
+        averageRGB(position + d.xy) - averageRGB(position - d.xy),
+        averageRGB(position + d.yx) - averageRGB(position - d.yx)
     ) / eps / 2.;
 }
 
-#define AngleNum 3
-#define SampNum 16
+const int angleCount = 3;
+const int sampleCount = 16;
 
 void main()
 {
-    vec2 pos = gl_FragCoord.xy + 4. * sin(TIME * vec2(1, 1.7)) * RENDERSIZE.y / 400.;
-    vec3 col = vec3(0);
-    vec3 col2 = vec3(0);
+    float scaleFactor = RENDERSIZE.y / 400.;
+
+    vec2 position = gl_FragCoord.xy + 4. * sin(TIME * vec2(1, 1.7)) * scaleFactor;
+    vec3 color1 = vec3(0);
+    vec3 color2 = vec3(0);
     float sum = 0.;
 
-    for (int i = 0; i < AngleNum; i++) {
-        float ang = TWO_PI / float(AngleNum) * (float(i) + 0.8);
-        vec2 v = polar2cart(vec2(ang, 1));
+    for (int i = 0; i < angleCount; i++) {
+        float angle = TWO_PI / float(angleCount) * (float(i) + 0.8);
+        vec2 vector = polar2cart(vec2(angle, 1));
+        vec2 perpendicularVector = vector.yx * vec2(1, -1);
 
-        for (int j = 0; j < SampNum; j++) {
-            vec2 dpos = v.yx * vec2(1, -1) * float(j) * RENDERSIZE.y / 400.;
-            vec2 dpos2 = v.xy * float(j * j) / float(SampNum) * 0.5 * RENDERSIZE.y / 400.;
+        for (int j = 0; j < sampleCount; j++) {
+            vec2 deltaPosition1 = perpendicularVector * float(j) * scaleFactor;
+            vec2 deltaPosition2 = vector * float(j * j) / float(sampleCount) * 0.5 * scaleFactor;
 
-            for (float s = -1.; s <= 1.; s += 2.) {
-                vec2 pos2 = pos + s * dpos + dpos2;
-                vec2 pos3 = pos + (s * dpos + dpos2).yx * vec2(1, -1) * 2.;
-               	vec2 g = getGrad(pos2, 0.4);
-               	float fact = dot(g, v) - 0.5 * abs(dot(g, v.yx * vec2(1, -1)));
-               	float fact2 = dot(normalize(g + vec2(EPSILON)), v.yx * vec2(1, -1));
+            for (float sgn = -1.; sgn <= 1.; sgn += 2.) {
+               	vec2 gradient = getGrad(position + (sgn * deltaPosition1 + deltaPosition2), 0.4);
 
-                fact = clamp(fact, 0., 0.05);
-                fact2 = abs(fact2);
+               	float fact = clamp(dot(gradient, vector) - 0.5 * abs(dot(gradient, perpendicularVector)), 0., 0.05);
+                fact *= 1. - float(j) / float(sampleCount);
+               	color1 += fact;
 
-                fact *= 1. - float(j) / float(SampNum);
-               	col += fact;
-               	col2 += fact2 * getColHT(pos3).xyz;
+               	float fact2 = abs(dot(normalize(gradient + vec2(EPSILON)), perpendicularVector));
+               	color2 += fact2 * getColHT(position + (sgn * deltaPosition1 + deltaPosition2).yx * vec2(1, -1) * 2.).rgb;
                	sum += fact2;
             }
         }
     }
-    col /= float(SampNum * AngleNum) * 0.75 / sqrt(RENDERSIZE.y);
-    col2 /= sum;
-    col.x *= 0.6 + 0.8 * random4(pos * 0.7).x;
-    col.x = 1. - col.x;
-    col.x *= col.x * col.x;
 
-    vec2 s = sin(pos.xy * 0.1 / sqrt(RENDERSIZE.y / 400.));
-    vec3 karo = vec3(1);
-    karo -= 0.5 * vec3(0.25, 0.1, 0.1) * dot(exp(-s*s * 80.), vec2(1));
-    float r = length(pos - RENDERSIZE.xy * 0.5) / RENDERSIZE.x;
-    float vign = 1. - r*r*r;
-    gl_FragColor = vec4(col.x * col2, 1);
+    color1 /= float(angleCount * sampleCount) * 0.75 / sqrt(RENDERSIZE.y);
+    color1.x *= 0.6 + 0.8 * random4(position * 0.7).x;
+    color1.x = 1. - color1.x;
+    color1.x *= color1.x * color1.x;
+
+    color2 /= sum;
+
+    gl_FragColor = vec4(color1.x * color2, 1);
 }
