3bf0f8690133835dca8bc39b8b1ebf0f312b2bc2
Nate Whetsell <nathan.whetsell@gmail.com>
2026-09-18 14:47:42 -0400

    Add inputs

--- MattiasCRT.fs
+++ MattiasCRT.fs
@@ -10,6 +10,22 @@
             "TYPE": "image"
         },
         {
+            "NAME": "horizontalScanDensity",
+            "LABEL": "Horizontal scan density",
+            "TYPE": "float",
+            "DEFAULT": 1.5,
+            "MAX": 5,
+            "MIN": 0
+        },
+        {
+            "NAME": "verticalScanSpacing",
+            "LABEL": "Vertical scan spacing",
+            "TYPE": "float",
+            "DEFAULT": 2,
+            "MAX": 5,
+            "MIN": 0
+        },
+        {
             "NAME": "zoomAmount",
             "LABEL": "Zoom amount",
             "TYPE": "float",
@@ -76,7 +92,7 @@ void main()
                   sin( 0.70 * TIME + uv.y * 29.) *
                   sin(0.3 + 0.33 * TIME + uv.y * 31.) *
                   0.0017;
-        vec2 northeast = vec2(uv.x + 0.001, uv.y + 0.001);
+        vec2 northeast = uv + 0.001;
         vec2 south = vec2(uv.x, uv.y - 0.002);
         vec2 west = vec2(uv.x - 0.002, uv.y);
         vec3 color = vec3(
@@ -88,10 +104,11 @@ void main()
         color *= vec3(pow(16. * uv.x * uv.y * (1. - uv.x) * (1. - uv.y), vignetteAmount));
         color *= vec3(0.95, 1.05, 0.95);
         color *= 2.8;
-        float scans = clamp(0.35 + 0.35 * sin(3.5 * TIME + uv.y * RENDERSIZE.y * 1.5), 0., 1.);
+        float scans = clamp(0.35 + 0.35 * sin(3.5 * TIME + uv.y * RENDERSIZE.y * horizontalScanDensity), 0., 1.);
         color *= vec3(0.4 + 0.7 * pow(scans, 1.7));
         color *= 1. + 0.01 * sin(110. * TIME);
-        color *= 1. - 0.65 * vec3(clamp((mod(gl_FragCoord.x, 2.) - 1.) * 2., 0., 1.));
+        // Vertical scan lines
+        color *= 1. - 0.65 * vec3(clamp((mod(gl_FragCoord.x, verticalScanSpacing) - 1.) * 2., 0., 1.));
         gl_FragColor = vec4(color, IMG_THIS_PIXEL(inputImage).a);
     }
 }
